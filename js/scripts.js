c66d0e1d886b817f3dd80312e2c84e7a8122f4a0
surduflorincodezilla <101145718+surduflorincodezilla@users.noreply.github.com>
2022-04-08 18:18:58 +0300

    Third Commit
    Third Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 150) {
+      $('.navbar').addClass('navbar-fixed');
+  } else {
+      $('.navbar').removeClass('navbar-fixed');
+  }
+});
+
 $('.top-banner').slick({
     dots: false,
     infinite: true,
@@ -41,7 +50,7 @@ $('.top-banner').slick({
     fade: true
 });
 
-if (window.matchMedia("(max-width: 767px)").matches) {
+/*if (window.matchMedia("(max-width: 767px)").matches) {
   $('#portfolio .row').slick({
     dots: false,
     infinite: true,
@@ -64,3 +73,79 @@ if (window.matchMedia("(max-width: 1024px)").matches) {
     fade: true
   });
 } 
+
+$('#portfolio .row').slick({
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: "unslick",
+      // unslick: true
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true
+      }
+    }
+  ]
+});*/
+
+
+var carousel = $('#portfolio .row');
+
+function showSliderScreen($screenWidth) {
+    if($screenWidth <= "768") {
+        if(!carousel.hasClass('slick-initalized')){
+            carousel.slick({
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 2
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1
+                        }
+                    }
+                ]
+            });
+        }
+    } else {
+        if (carousel.hasClass('slick-initialized')) {
+            carousel.unslick();
+        }
+    }
+}
+
+var screenWidth = $(window).width();
+$(window).ready(showSliderScreen(screenWidth)).resize(function(){
+    var screenWidth = $(window).width();
+    showSliderScreen(screenWidth);
+});
